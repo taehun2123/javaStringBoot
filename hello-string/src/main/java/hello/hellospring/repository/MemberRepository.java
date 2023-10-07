1f847fe6d715c526b568b0a09d6c6582e6ccb2df
taehun2123 <pyoneng_@naver.com>
2023-10-07 19:49:03 +0900

    이해를 위한 관련 개념과 주석 추가

--- hello-string/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-string/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -4,12 +4,13 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; // 값이 존재할 수도 있고 존재하지 않을 수도 있는 상황을 나타내는 컨테이너 객체
+//null 관련 예외를 방지하고 코드를 안전하게 만듬.
 
 public interface MemberRepository {
     Member save(Member member); //회원 저장
-    Optional<Member> findById(Long id); //찾기 ID
-    Optional<Member> findByName(String name); //찾기 이름
+    Optional<Member> findById(Long id); //찾기 ID (Null 예외처리를 위한 Optional 클래스 삽입
+    Optional<Member> findByName(String name); //찾기 이름 (Null 예외처리를 위한 Optional 클래스 삽입
     List<Member> findAll(); //저장된 모든 리스트 반환
 
 }
--- hello-string/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-string/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,30 +6,30 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L; //sequence => 0, 1, 2 생성해주는 애(Atomic long)
 
     @Override
-    public hello.hellospring.domain.Member save(hello.hellospring.domain.Member member) {
+    public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
     }
 
     @Override
-    public Optional<hello.hellospring.domain.Member> findById(Long id) {
+    public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
-    public Optional<hello.hellospring.domain.Member> findByName(String name) {
+    public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().equals(name))
+                .filter(member -> member.getName().equals(name)) //람다 사용 () -> ()
                 .findAny();
     }
 
     @Override
-    public List<hello.hellospring.domain.Member> findAll() {
+    public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
 
